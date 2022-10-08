099cf9eb05ec560f1daec409f0b015b17b45514f
Rostislav Alekseev <mr.rostik.01@gmail.com>
2022-10-08 14:34:22 +0200

    добавлен InputComponent

---
+++ src/components/UI/InputComponent.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+import search_icon from "../../assets/icons/search_icon.svg";
+import search_icon_active from "../../assets/icons/search_icon_active.svg";
+import filter_icon from "../../assets/icons/filter_icon.svg";
+
+function InputComponent() {
+  const [activeSearch, setActiveSearch] = useState("");
+
+  return (
+    <div className="mx-4 relative">
+      <div className="flex absolute inset-y-0 items-center pl-3">
+        <img src={search_icon} alt="search_icon" />
+      </div>
+      <div
+        className={
+          "flex absolute inset-y-0 items-center pl-3 " +
+          (activeSearch == "" ? "hidden" : "")
+        }
+      >
+        <img src={search_icon_active} alt="search_icon" />
+      </div>
+      <div className="flex absolute inset-y-0 right-0 items-center pr-3">
+        <img src={filter_icon} alt="search_icon" />
+      </div>
+      <input
+        type="text"
+        className={
+          "rounded-2xl h-10 bg-[#F7F7F8] w-full pl-11 font-medium text-[15px] focus:outline-none caret-custom-purple " +
+          activeSearch
+        }
+        placeholder="Введи имя, тег, почту..."
+        onFocus={() => {
+          setActiveSearch("active");
+        }}
+        onBlur={() => {
+          setActiveSearch("");
+        }}
+      />
+    </div>
+  );
+}
+
+export default InputComponent;
--- src/components/UI/TopAppBarComponent.jsx
+++ src/components/UI/TopAppBarComponent.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-
-import search_icon from "../../assets/icons/search_icon.svg"
-import filter_icon from "../../assets/icons/filter_icon.svg"
+import InputComponent from './InputComponent'
 
 function TopAppBarComponent() {
   return (
@@ -11,19 +9,11 @@ function TopAppBarComponent() {
         <h1 className='font-bold text-2xl'>Поиск</h1>
       </div>
 
-      <div className='mx-4 relative'>
-        <div className='flex absolute inset-y-0 items-center pl-3'>
-          <img src={search_icon} alt="search_icon" />
-        </div>
-        <div className='flex absolute inset-y-0 right-0 items-center pr-3'>
-          <img src={filter_icon} alt="search_icon" />
-        </div>
-        <input type="text" className='rounded-2xl h-10 bg-[#F7F7F8] w-full pl-11 font-medium text-[15px]' placeholder='Введи имя, тег, почту...' />
-      </div>
+      <InputComponent />
 
       <div>
         <ul className='flex justify-start ml-4 h-9 items-center font-medium text-[15px] text-[#97979B]'>
-          <li className='px-3 h-full flex items-center border-b-2 border-[#6534FF] font-semibold text-black'>Все</li>
+          <li className='px-3 h-full flex items-center border-b-2 border-custom-purple font-semibold text-black'>Все</li>
           <li className='px-3 h-full flex items-center'>Designers</li>
           <li className='px-3 h-full flex items-center'>Analysts</li>
           <li className='px-3 h-full flex items-center'>Managers</li>
