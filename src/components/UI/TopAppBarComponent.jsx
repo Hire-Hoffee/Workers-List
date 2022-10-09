9ddcdbf9e6618754e92c1c658997d81362da6b8b
Rostislav Alekseev <mr.rostik.01@gmail.com>
2022-10-09 21:40:09 +0200

    загрузка определенных пользователей

--- src/components/UI/TopAppBarComponent.jsx
+++ src/components/UI/TopAppBarComponent.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 import InputComponent from "./InputComponent";
 
@@ -13,12 +14,45 @@ function TopAppBarComponent() {
 
       <div>
         <ul className="flex justify-start ml-4 h-9 items-center font-medium text-[15px] text-[#97979B] overflow-x-auto">
-          <li className="px-3 h-full flex items-center border-b-2 border-custom-purple font-semibold text-black">Все</li>
-          <li className="px-3 h-full flex items-center">Designers</li>
-          <li className="px-3 h-full flex items-center">Analysts</li>
-          <li className="px-3 h-full flex items-center">Managers</li>
-          <li className="px-3 h-full flex items-center">iOS</li>
-          <li className="px-3 h-full flex items-center">Android </li>
+          <li className="px-3 h-full flex items-center">
+            <NavLink to={'/main/all'}>Все</NavLink>
+          </li>
+          <li className="px-3 h-full flex items-center">
+            <NavLink to={'/main/design'}>Дизайн</NavLink>
+          </li>
+          <li className="px-3 h-full flex items-center">
+            <NavLink to={'/main/analytics'}>Аналитика</NavLink>
+          </li>
+          <li className="px-3 h-full flex items-center">
+            <NavLink to={'/main/management'}>Managers</NavLink>
+          </li>
+          <li className="px-3 h-full flex items-center">
+            <NavLink to={'/main/ios'}>iOS</NavLink>
+          </li>
+          <li className="px-3 h-full flex items-center">
+            <NavLink to={'/main/android'}>Android</NavLink>
+          </li>
+          <li className="px-3 h-full flex items-center">
+            <NavLink to={'/main/frontend'}>Frontend</NavLink>
+          </li>
+          <li className="px-3 h-full flex items-center">
+            <NavLink to={'/main/qa'}>QA</NavLink>
+          </li>
+          <li className="px-3 h-full flex items-center">
+            <NavLink to={'/main/back_office'}>Бэк-офис</NavLink>
+          </li>
+          <li className="px-3 h-full flex items-center">
+            <NavLink to={'/main/hr'}>HR</NavLink>
+          </li>
+          <li className="px-3 h-full flex items-center">
+            <NavLink to={'/main/pr'}>PR</NavLink>
+          </li>
+          <li className="px-3 h-full flex items-center">
+            <NavLink to={'/main/backend'}>Backend</NavLink>
+          </li>
+          <li className="px-3 h-full flex items-center">
+            <NavLink to={'/main/support'}>Техподдержка</NavLink>
+          </li>
         </ul>
         <hr />
       </div>
